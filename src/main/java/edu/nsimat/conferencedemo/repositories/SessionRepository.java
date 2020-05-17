c052de78ce220f3eff637e44c940d5f0fb4e6160
Michel Nsimba Matondo <nsimba_matondo@yahoo.fr>
2020-05-17 11:14:17 +0200

    Adding the conference-demo Spring Boot project.

--- src/main/java/edu/nsimat/conferencedemo/repositories/SessionRepository.java
+++ src/main/java/edu/nsimat/conferencedemo/repositories/SessionRepository.java
@@ -3,5 +3,5 @@ package edu.nsimat.conferencedemo.repositories;
 import edu.nsimat.conferencedemo.models.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SessionRepository extends JpaRepository<Session, Long> {
+public interface SessionRepository extends JpaRepository<Session,Long> {
 }
